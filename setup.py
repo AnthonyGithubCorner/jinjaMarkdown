5edfbe6bba0dff0c59af44dfd8287d385a9ae63f
CSIProjectDemo <citizenscienceinc@gmail.com>
2021-08-21 20:15:07 -0400

    fixed link bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jinjaMarkdown",
-    version="0.0.7",
+    version="0.0.8",
     author="Anthony Lazar",
     author_email="alazar@uwaterloo.ca",
     description="A package containing a markdown filter for jinja2",
